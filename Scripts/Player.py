045dce8df23ff75566fb71972f968c7b19a3601f
Bruce <cbaal@korea.com>
2018-11-18 20:26:45 +0900

    리소스 추가 & 발사 구현
    매끄럽지 못한 리소스 수정
    발사 구현

--- Scripts/Player.py
+++ Scripts/Player.py
@@ -2,7 +2,7 @@ import Game_FrameWork
 import random
 from pico2d import *
 from math import *
-#from ball import Ball
+from Player_Bullet import Player_Bullet
 
 import Game_World
 
@@ -17,13 +17,13 @@ RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
-# Boy Action Speed
+# Player Action Speed
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 5
 
 # Player Event
-RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, TOP_UP,TOP_DOWN, GROUND, SPACE = range(8)
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, TOP_UP,TOP_DOWN, GROUND, SPACE, X = range(9)
 
 key_event_table = {
     (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
@@ -32,7 +32,8 @@ key_event_table = {
     (SDL_KEYUP, SDLK_LEFT): LEFT_UP,
     (SDL_KEYUP, SDLK_UP): TOP_UP,
     (SDL_KEYDOWN, SDLK_UP): TOP_DOWN,
-    (SDL_KEYDOWN, SDLK_SPACE): SPACE
+    (SDL_KEYDOWN, SDLK_SPACE): SPACE,
+    (SDL_KEYDOWN, SDLK_x): X
 }
 
 
@@ -42,7 +43,8 @@ class IdleState:
 
     @staticmethod
     def enter(player, event):
-        player.image = load_image("C:\\GitHub\\2DGP_TermProject\\Resources\\Player\\Idle\\Player_Idle2.png")
+        player.image = load_image("C:\\GitHub\\2DGP_TermProject\\Resources\\Player\\Idle\\Player_Idle.png")
+        player.frame = 0
         if event == RIGHT_DOWN:
             player.velocity += RUN_SPEED_PPS
         elif event == LEFT_DOWN:
@@ -55,41 +57,46 @@ class IdleState:
             player.image = load_image("C:\\GitHub\\2DGP_TermProject\\Resources\\Player\\Idle\\Player_Idle_Up.png")
             player.updown = 1
         elif event == TOP_UP:
-            player.image = load_image("C:\\GitHub\\2DGP_TermProject\\Resources\\Player\\Idle\\Player_Idle2.png")
+            player.image = load_image("C:\\GitHub\\2DGP_TermProject\\Resources\\Player\\Idle\\Player_Idle.png")
             player.updown = 0
-
 
     @staticmethod
     def exit(player, event):
-        pass
+        if event == X:
+            player.fire_bullet()
 
     @staticmethod
     def do(player):
-        global timer
-        timer = get_time()
-        player.frame = (player.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * Game_FrameWork.frame_time) % 5
+        if player.isIdleFrameIncrease:
+            player.frame += (FRAMES_PER_ACTION * ACTION_PER_TIME * Game_FrameWork.frame_time) % 5
+            if player.frame >= 4:
+                player.isIdleFrameIncrease = False
+        else:
+            player.frame -= (FRAMES_PER_ACTION * ACTION_PER_TIME * Game_FrameWork.frame_time) % 5
+            if player.frame <= 0:
+                player.isIdleFrameIncrease = True
+
 
     @staticmethod
     def draw(player):
         if player.dir == 1:
             if player.updown == 1:
-                player.image.clip_draw(int(player.frame) * 105, 175, 105, 175, player.x, player.y)
+                player.image.clip_draw(int(player.frame) * 134, 0, 134, 161, player.x, player.y)
             else:
-                player.image.clip_draw(int(player.frame) * 131, 161, 131, 161, player.x, player.y)
+                player.image.clip_draw(int(player.frame) * 134, 0, 134, 161, player.x, player.y)
         else:
             if player.updown == 1:
-                player.image.clip_draw(int(player.frame) * 105, 0, 105, 175, player.x, player.y)
+                player.image.clip_draw(int(player.frame) * 134, 0, 134, 161, player.x, player.y)
             else:
-                player.image.clip_draw(int(player.frame) * 131, 0, 131, 161, player.x, player.y)
+                player.image.clip_draw(int(player.frame) * 133, 161, 133, 158, player.x, player.y)
 
 
 
 class RunState:
 
-
     @staticmethod
     def enter(player, event):
-        player.image = load_image("C:\\GitHub\\2DGP_TermProject\\Resources\\Player\\Run\\Player_Run2.png")
+        player.image = load_image("C:\\GitHub\\2DGP_TermProject\\Resources\\Player\\Run\\Player_Run.png")
 
         if event == RIGHT_DOWN:
             player.velocity += RUN_SPEED_PPS
@@ -103,21 +110,24 @@ class RunState:
 
     @staticmethod
     def exit(player, event):
-        if event == SPACE:
-            player.fire_ball()
+        if event == X:
+            player.fire_bullet()
 
     @staticmethod
     def do(player):
-        player.frame = (player.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * ACTION_PER_TIME * Game_FrameWork.frame_time) % 16
+        if player.dir == 1:
+            player.frame = (player.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * ACTION_PER_TIME * Game_FrameWork.frame_time) % 16
+        else:
+            player.frame = (player.frame - (FRAMES_PER_ACTION * ACTION_PER_TIME * ACTION_PER_TIME * Game_FrameWork.frame_time)) % 16
         player.x += player.velocity * Game_FrameWork.frame_time
         player.x = clamp(25, player.x, 1600 - 25)
 
     @staticmethod
     def draw(player):
         if player.dir == 1:
-            player.image.clip_draw(int(player.frame) * 175, 0, 175, 162, player.x, player.y)
+            player.image.clip_draw(int(player.frame) * 144, 0, 144, 162, player.x, player.y)
         else:
-            player.image.clip_draw(int(player.frame) * 175, 162, 175, 162, player.x, player.y)
+            player.image.clip_draw(int(player.frame) * 144, 162, 144, 162, player.x, player.y)
 
 
 class JumpState:
@@ -139,7 +149,8 @@ class JumpState:
 
     @staticmethod
     def exit(player, event):
-        pass
+        if event == SPACE:
+            player.fire_bullet()
 
     @staticmethod
     def do(player):
@@ -167,29 +178,28 @@ class JumpState:
 
 
 next_state_table = {
-    IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState, RIGHT_DOWN: RunState, LEFT_DOWN: RunState, TOP_UP: IdleState, TOP_DOWN: IdleState , SPACE: JumpState, GROUND: IdleState},
-    RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState, LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState, TOP_UP: IdleState, TOP_DOWN: IdleState , SPACE: JumpState, GROUND: RunState},
-    JumpState: {RIGHT_UP: JumpState, LEFT_UP: JumpState, LEFT_DOWN: JumpState, RIGHT_DOWN: JumpState, TOP_UP: JumpState, TOP_DOWN: JumpState , SPACE: JumpState, GROUND :RunState},
+    IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState, RIGHT_DOWN: RunState, LEFT_DOWN: RunState, TOP_UP: IdleState, TOP_DOWN: IdleState , SPACE: JumpState, GROUND: IdleState, X: IdleState},
+    RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState, LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState, TOP_UP: IdleState, TOP_DOWN: IdleState , SPACE: JumpState, GROUND: RunState, X: RunState},
+    JumpState: {RIGHT_UP: JumpState, LEFT_UP: JumpState, LEFT_DOWN: JumpState, RIGHT_DOWN: JumpState, TOP_UP: JumpState, TOP_DOWN: JumpState , SPACE: JumpState, GROUND :RunState, X: JumpState},
 }
 
 class Player:
 
     def __init__(self):
         self.x, self.y = 1900 // 2, 90
-        self.image = load_image("C:\\GitHub\\2DGP_TermProject\\Resources\\Player\\Idle\\Player_Idle2.png")
-#        self.font = load_font('ENCR10B.TTF', 32)
+        self.image = load_image("C:\\GitHub\\2DGP_TermProject\\Resources\\Player\\Idle\\Player_Idle.png")
         self.dir = 1
         self.updown = 0
+        self.isIdleFrameIncrease = True
         self.velocity = 0
         self.frame = 0
         self.event_que = []
         self.cur_state = IdleState
         self.cur_state.enter(self, None)
 
-    def fire_ball(self):
-        pass
-        #ball = Ball(self.x, self.y, self.dir*3)
-        #Game_World.add_object(ball, 1)
+    def fire_bullet(self):
+        player_bullet = Player_Bullet(self.x, self.y, self.dir*10)
+        Game_World.add_object(player_bullet, 1)
 
     def add_event(self, event):
         self.event_que.insert(0, event)
@@ -204,7 +214,6 @@ class Player:
         print(self.cur_state, self.image)
     def draw(self):
         self.cur_state.draw(self)
-#        self.font.draw(self.x - 60, self.y + 50, '(Time: %3.2f)' % get_time(), (255, 255, 0))
 
     def handle_event(self, event):
         if (event.type, event.key) in key_event_table:
---
+++ Scripts/Player_Bullet.py
@@ -0,0 +1,28 @@
+from pico2d import *
+import Game_World
+import Game_FrameWork
+
+# Player_Bullet Action Speed
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+class Player_Bullet:
+    image = None
+
+    def __init__(self, x = 400, y = 300, velocity = 1):
+        if Player_Bullet.image == None:
+            Player_Bullet.image = load_image('C:\\GitHub\\2DGP_TermProject\\Resources\\Player\\Bullet\\Player_Bullet.png')
+        self.x, self.y, self.velocity = x, y, velocity
+        self.frame = 0
+
+
+    def draw(self):
+        self.image.clip_draw(int(self.frame) * 80, 0, 80, 51, self.x, self.y)
+
+    def update(self):
+        self.x += self.velocity
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * Game_FrameWork.frame_time) % 4
+
+        if self.x < 25 or self.x > 1600 - 25:
+            Game_World.remove_object(self)
