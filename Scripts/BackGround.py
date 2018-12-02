1980af99305857ec4046df81ca959da45b75230e
Bruce <cbaal@korea.com>
2018-12-02 16:43:19 +0900

    배경음 구현

--- Scripts/BackGround.py
+++ Scripts/BackGround.py
@@ -3,6 +3,9 @@ from pico2d import *
 class BackGround:
     def __init__(self):
         self.image = load_image('C:\\GitHub\\2DGP_TermProject\\Resources\\BackGround\\background.png')
+        self.background_music = load_music("C:\\GitHub\\2DGP_TermProject\\Resources\\Sound\\Background_music.mp3")
+        self.background_music.set_volume(64)
+        self.background_music.repeat_play()
 
     def update(self):
         pass
